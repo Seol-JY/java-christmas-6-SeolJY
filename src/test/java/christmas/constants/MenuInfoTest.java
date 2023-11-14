2752863b33b6d954d23c5de9e9282f106c314722
Seol_JY <wlsdud5654@gmail.com>
2023-11-14 21:18:10 +0900

    test(MenuInfo): MenuInfo에 대한 테스트 작성
    테스트 내역:
    - 메뉴 이름을 통해 MenuInfo를 가져옴
    - 존재하지 않는 메뉴 이름으로 MenuInfo를 가져올 시 예외가 발생한다.

---
+++ src/test/java/christmas/constants/MenuInfoTest.java
@@ -0,0 +1,32 @@
+package christmas.constants;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("MenuInfo 테스트")
+class MenuInfoTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"양송이수프", "타파스", "시저샐러드", "티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타", "초코케이크", "아이스크림", "제로콜라",
+            "레드와인", "샴페인"})
+    @DisplayName("메뉴 이름으로 MenuInfo를 가져올 수 있다.")
+    void getMenuInfoByName(String menuName) {
+        // Arrange & Act
+        MenuInfo menuInfo = MenuInfo.getMenuInfoByName(menuName);
+
+        // Assert
+        assertThat(menuInfo.getName()).isEqualTo(menuName);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"InvalidMenu", "InvalidName", "InvalidItem"})
+    @DisplayName("존재하지 않는 메뉴 이름으로 MenuInfo를 가져오려고 하면 예외가 발생한다.")
+    void getMenuInfoByInvalidName(String invalidMenuName) {
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> MenuInfo.getMenuInfoByName(invalidMenuName));
+    }
+}
+
