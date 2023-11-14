7f9eeea5375a2f47559a3d2978a9d9755b38a049
Seol_JY <wlsdud5654@gmail.com>
2023-11-15 02:18:31 +0900

    feat(OrderItems): 메뉴와 메뉴 수량을 저장하는 기능 구현
    상세 내역:
    - 메뉴 수량 자연수 판별
    - 중복 메뉴에 대한 예외
    - 수량 20개 초과시 예외

---
+++ src/main/java/christmas/domain/OrderItems.java
@@ -0,0 +1,62 @@
+package christmas.domain;
+
+import christmas.constants.ErrorMessage;
+import christmas.constants.MenuCategory;
+import christmas.constants.MenuInfo;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+
+public final class OrderItems {
+    private static final int MAX_TOTAL_ITEM_COUNT = 20;
+    private final EnumMap<MenuInfo, Integer> items;
+
+    private OrderItems(EnumMap<MenuInfo, Integer> items) {
+        this.items = new EnumMap<>(items);
+    }
+
+    public static OrderItems create() {
+        return new OrderItems(new EnumMap<>(MenuInfo.class));
+    }
+
+    public OrderItems addItem(MenuInfo menu, int quantity) {
+        validateDuplicate(menu);
+        validateQuantity(quantity);
+        validateTotalItemCount(quantity);
+
+        EnumMap<MenuInfo, Integer> newItems = new EnumMap<>(items);
+        newItems.put(menu, quantity);
+
+        return new OrderItems(newItems);
+    }
+
+    public boolean containsOnlyBeverages() {
+        return items.keySet().stream().allMatch(MenuCategory.BEVERAGE::containsMenu);
+    }
+
+    public Map<MenuInfo, Integer> getItems() {
+        return Collections.unmodifiableMap(items);
+    }
+
+    private void validateDuplicate(MenuInfo menu) {
+        if (items.containsKey(menu)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+        }
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+        }
+    }
+
+    private void validateTotalItemCount(int quantity) {
+        if (getTotalItemCount() + quantity > MAX_TOTAL_ITEM_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+        }
+    }
+
+    private int getTotalItemCount() {
+        return items.values().stream().mapToInt(Integer::intValue).sum();
+    }
+}
